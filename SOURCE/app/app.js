037127c031d7b9ecd5faeb63cd271911a97470bb
Slavko_V <slavko.vuletic92@gmail.com>
2021-03-18 11:20:11 +0100

    first run

---
+++ SOURCE/app/app.js
@@ -0,0 +1,64 @@
+//╔═══════════════════════════════════════════════════════════════════════════════╗
+//║ ♠ File Name: app.js                                                           ║
+//║ ↔ Location: <: public_root :>/ app.js                                         ║
+//║ Σ Description:                                                                ║
+//║     → In case this lives more than few days, better have something prepared   ║
+//╟       than few days, better have something prepared have as   ╔═══════════════╣
+//║       than few days, better have something prepared.          ║ √ 23.01.2021. ║
+//╚═══════════════════════════════════════════════════════════════╩═══════════════╝
+
+
+function footerFunction() {
+    console.log('FooterFunction();')
+}
+
+function aboutFunction() {
+    console.log('aboutFunction();')
+}
+
+function heroFunction() {
+    console.log('heroFunction();')
+}
+
+function newConsultationFunction() {
+    console.log('newConsultationFunction();')
+}
+
+function testimonialsFunction() {
+    console.log('testimonialsFunction()')
+}
+// Usage.
+
+var testItems = [
+    {
+        'className': '.desktop-footer',
+        'done': false,
+        call() { footerFunction() }
+    },
+    {
+        'className': '.about-us',
+        'done': false,
+        call() { aboutFunction() }
+    },
+    {
+        'className': '.lp-header-overlay',
+        'done': false,
+        call() { heroFunction() }
+    },
+    {
+        'className': '.new_consultation ',
+        'done': false,
+        call() { newConsultationFunction() }
+    },
+    {
+        'className': '.testimonials ',
+        'done': false,
+        call() { testimonialsFunction() }
+    }
+]
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+//▌   ! END OF FILE !            ▐▀▀▀         ▐▀▀▀             ! END OF FILE !    ▐
+//▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄      ▐▀▀▀  ▐▀▀▀▀▌ ▐▀▀▀         ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▐
+//▌ ♠ File Name: app.js   ▐      ▐▄▄▄  ▐▄▄▄▄▌ ▐            ▌  ☺ Author: Slavko V. ▐
+//▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
