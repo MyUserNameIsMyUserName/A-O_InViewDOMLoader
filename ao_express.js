8ce2c9b5707a56ba17d8c863138d5335e26bc4e0
MyUserNameIsMyUserName <slavko.vuletic92@gmail.com>
2021-05-15 05:25:30 +0200

    yea dependencie and reading onf demo env file

--- ao_express.js
+++ ao_express.js
@@ -1,10 +1,13 @@
 // Express
+require('dotenv').config();
 const compression = require("compression");
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const PORT = 8080;
+const LOCAL_PROTOCOL = process.env.LOCAL_PROTOCOL;
+const LOCAL_HOST = process.env.LOCAL_HOST;
+const LOCAL_PORT = process.env.LOCAL_PORT;
 
 const STATIC = path.resolve(__dirname, "PUBLIC");
 const INDEX = path.resolve(STATIC, "index.html");
@@ -20,11 +23,10 @@ app.use(express.static(STATIC));
 
 // All GET request handled by INDEX file
 app.get("*", function (req, res) {
-    req.headers["Bypass-Tunnel-Reminder"] = "YEAdoIT";
     res.sendFile(INDEX);
 });
 
 // Start server
 app.listen(PORT, function () {
-    console.log("Server up and running on http://localhost:" + PORT + "/");
+    console.log("Server up and running on " + LOCAL_PROTOCOL + "://" + LOCAL_HOST + ":" + LOCAL_PORT + "/");
 });
